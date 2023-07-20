a58ade0e0b29a992e87a2ed5a5656059380d6bba
wangli.007 <wangli.007@bytedance.com>
2023-07-21 00:23:31 +0800

    fix: path

--- src/app.service.ts
+++ src/app.service.ts
@@ -22,6 +22,7 @@ export class AppService {
   async deploy(res: Response, path: string) {
     const date = new Date();
     const fileName = `${date.getFullYear()}${date.getMonth()}${date.getDate()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    await shell.cd(path);
     await shell.exec(`${path}/build.sh ${fileName}`);
     const file = createReadStream(join(`${path}/log`, `${fileName}.txt`));
     file.pipe(res as any);
